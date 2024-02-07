dd4925f31cba88beba089036105aa85852dd8b8b
Idris Noor <idreesnoor150@gmail.com>
2024-02-07 13:09:34 +0300

    tested with the address

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import  "dotenv/config";
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+// const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  defaultNetwork: "sepolia",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+
+      accounts: [],
     }
+   
   }
 };
 
